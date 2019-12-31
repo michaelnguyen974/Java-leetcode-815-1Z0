0f323a95969d386d1c9ad92198a7290a5eafae78
Michael Nguyen <mnguyen@deloitte.co.uk>
2019-12-31 11:10:42 +0000

    Added unit test for sorting a List

--- src/test/java/org/example/leetcode/AppTest.java
+++ src/test/java/org/example/leetcode/AppTest.java
@@ -2,20 +2,29 @@ package org.example.leetcode;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
+import org.example.leetcode.App;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        // assertTrue( true );
-        assertEquals(true, false);
+    public void sortListWithTwoNumbers() {
+       App app = new App();
+	   int[] intArray = {5,2,3,1};
+	   List solution = app.sortArray(intArray);
+	   List<Integer> expected = List.of(1,2,3,5); // List.of is a factory method since Java 9
+	   assertEquals(expected, solution);
     }
+
+    // @Test
+    // public void shouldAnswerWithTrue()
+    // {
+    //     assertEquals(true, false);
+    // }
 }
